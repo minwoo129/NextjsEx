f576cb4e214cef676f7a5566136f1384d39e80af
minwoo129 <key0129mw@naver.com>
2023-02-24 23:53:59 +0900

    navbar 스타일 속성 적용

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+  const router = useRouter();
   return (
     <nav>
-      <Link href="/">home</Link>
-      <Link href="/about">about</Link>
+      <Link href="/" legacyBehavior>
+        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>home</a>
+      </Link>
+      <Link href="/about" legacyBehavior>
+        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+          About
+        </a>
+      </Link>
     </nav>
   );
 }
