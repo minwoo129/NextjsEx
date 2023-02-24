16042d3b68be13c393360be414ddd86c6f8fc164
minwoo129 <key0129mw@naver.com>
2023-02-24 23:46:37 +0900

    직전 커밋 수정
    - nextjs 공식문서 확인 결과 현재 사용중인 nextjs 13 버전에서는
      노마드 코더와 같이 Link 태그 내에 anchor 태그를 적용하는 것이 유효하지
      않다고 함.
    - 작성한 코드처럼 Link 태그 내에 legacyBehavior 속성을 추가해주거나, 내부에
      anchor 태그를 적용하지 않는 방식으로 적용하면 된다고 함

--- nextjs-intro/components/NavBar.js
+++ nextjs-intro/components/NavBar.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <nav>
-      <Link href="/">
+      <Link href="/" legacyBehavior>
         <a>home</a>
       </Link>
-      <Link href="/about">
-        <a href="/about">about</a>
+      <Link href="/about" legacyBehavior>
+        <a>about</a>
       </Link>
     </nav>
   );
