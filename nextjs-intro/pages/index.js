eabcb8b2385deb00189147228904abaa12b32a9e
minwoo129 <key0129mw@naver.com>
2023-02-25 15:17:33 +0900

    App Component 적용 예제
    - Nextjs가 모든 페이지를 렌더링할 수 있게 하는 일종의 컴포넌트의 청사진
    - 기본으로 프레임워크 내에 포함되어 있지만, 커스터마이징하기 위해서는 pages 폴더에 _app.js라는 파일을 추가해줘야 한다.
       => NextJS는 렌더링하기 전에 반드시 _app.js 파일을 먼저 보기 때문

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -3,14 +3,7 @@ import NavBar from "../components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1>hello</h1>
-      {/* NavBar의 a 태그에 영향 X */}
-      <style jsx global>{`
-        a {
-          color: blue;
-        }
-      `}</style>
     </div>
   );
 }
