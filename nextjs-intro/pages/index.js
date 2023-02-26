033be0a4280a699b0076cb571510b5020e9cdf7d
minwoo129 <key0129mw@naver.com>
2023-02-26 12:56:36 +0900

    Head 모듈화

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,11 +1,10 @@
 import Head from "next/head";
+import Seo from "../components/Seo";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Home | Next Movies</title>
-      </Head>
+      <Seo title="Home" />
       <h1>hello</h1>
     </div>
   );
