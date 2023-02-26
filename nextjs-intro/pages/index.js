72e7f78e7a9ee969bd745c392e5ddbd22c4fda40
minwoo129 <key0129mw@naver.com>
2023-02-26 14:05:05 +0900

    Server Side 함수 추가
    - getServerSideProps라고 작성한 함수는 무조건 서버에서만 돌아가게 된다.
       -> 클라이언트에서는 보이지 않게 됨.
    - _app.js에서 props로 Component와 pageProps를 받으면서 자식 컴포넌트 페이지에서는
      props로 응답값을 전달받을 수 있게 된다.

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
+export default function Home({ results }) {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
@@ -12,8 +12,7 @@ export default function Home() {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => {
+      {results?.map((movie) => {
         return (
           <div className="movie" key={movie.id}>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
@@ -44,4 +43,18 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// 이 함수 내에서 작성한 코드는 무조건 서버에서 돌아간다.
+// 함수의 이름이 정말 중요!!!
+// 클라이언트에서 보여지지 않게 됨
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
